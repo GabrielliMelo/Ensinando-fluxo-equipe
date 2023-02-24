27a8f72e40aafd0527fc9b97c3375375d8ffbe69
ASUS_X515\iurif <iurifreire.18@gmail.com>
2023-02-24 19:17:15 -0300

    Adição da funcionalidade Calculadora

---
+++ calculadora.js
@@ -0,0 +1,15 @@
+function adicionar(a, b) {
+    console.log(a + b);
+};
+
+function subtrair(a, b) {
+    console.log(a - b);
+};
+
+function multiplicar(a, b) {
+    console.log(a * b);
+};
+
+function dividir(a, b) {
+    console.log(a / b);
+};
